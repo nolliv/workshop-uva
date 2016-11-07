718b120f0f85320fdbef7838bd5f629a862a2b10
Silas Villon <silas.villon@corp.globo.com>
2016-11-07 20:06:05 -0200

    meu primeeeeeiro commit

---
+++ main.go
@@ -0,0 +1,37 @@
+// Comentários iguais aos do C
+/* assim também */
+
+// Organização do código por pacotes
+package main
+
+import "fmt"
+
+func main() {
+	// exported em maiúsculo
+	// UNICODE ALL THINGS!!!!!!1!!
+	fmt.Println("Hello, Modafukás!")
+}
+
+// O Tipo é a frente do nome!
+func add(x int, y int) int {
+	return x + y
+}
+
+// Pode omitir tipo repetido
+func add2(x, y int) int {
+	return x + y
+}
+
+// multiple return values
+func swap(x, y string) (string, string) {
+	return y, x
+}
+
+// variable declaration
+func printbool() {
+	// inicializado com zero value(false)
+	var x bool
+	y := true
+	var z = true
+	fmt.Print(x, y, z) // faltou o z!
+}
---
+++ order.md
@@ -0,0 +1,37 @@
+topics
+======
+sintaxe
+ - loops
+ - conditionals
+ - slices
+ - maps
+ - structs
+ - functions
+ - errors are values
+
+unicode support
+tooling
+unused stuff is sintax errors
+instalação: GOPATH
+
+
+ferramentas oficiais
+open-source
+ - go get
+ - go test
+ - goimports
+ - gofmt
+Deploy
+ - cross compilation
+ - static linkage
+ - fast compilation times
+ - .so generation
+
+App
+
+order
+=====
+introdução
+ - quem sou eu (qualificação)
+ - objetivo
+ - o que será passado
